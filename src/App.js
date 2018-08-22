5391b16a05bed61cec3b2bea4923261cf3ff13b8
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-08-22 12:03:48 -0300

    Websocket simples acoplado ao react

--- src/App.js
+++ src/App.js
@@ -3,6 +3,68 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      msg: ''
+    }
+  }
+
+  componentDidMount() {
+    this.wsConnect()
+  }
+
+  wsConnect() {
+    this.state.connection = new WebSocket('ws://127.0.0.1:3001')
+
+    this.state.connection.onopen = function () {
+      console.log('WS OPENED!')
+    }
+
+    this.state.connection.onerror = function (error) {
+      alert('WS ERROR!')
+    }
+
+    this.state.connection.onmessage = function (message) {
+      // try to decode json (I assume that each message
+      // from server is json)
+      try {
+        var json = JSON.parse(message.data)
+        alert(json)
+      } catch (e) {
+        alert('SERVER NAO RETORNOU JSON')
+      }
+
+      debugger;
+      if (json.type === 'color') {
+      } else if (json.type === 'history') { // entire message history
+        for (var i=0; i < json.data.length; i++) {
+        console.log(json.data[i].author, json.data[i].text,
+            json.data[i].color, new Date(json.data[i].time));
+        }
+      } else if (json.type === 'message') { // it's a single message
+        console.log(json.data.author, json.data.text,
+                   json.data.color, new Date(json.data.time));
+      } else {
+        console.log('Hmm..., I\'ve never seen JSON like this:', json);
+      }
+    }
+  }
+
+  onChange(e) {
+    this.setState({
+      msg: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.state.connection.send(this.state.msg);
+    this.setState({
+      msg: ''
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +72,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>
+            Msg:
+            <input type="text" value={this.state.msg} onChange={this.onChange.bind(this)} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
