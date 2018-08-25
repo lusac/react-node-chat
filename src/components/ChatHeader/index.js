c70880a24e5bb46cdd8fa40644a5072ed1db9696
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-08-25 09:26:39 -0300

    Criando chat apartir da listagem lateral

--- src/components/ChatHeader/index.js
+++ src/components/ChatHeader/index.js
@@ -2,18 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default class ChatHeader extends React.Component {
-  createRoom() {
-    if (this.props.socket) {
-      this.props.socket.emit('create room', 'xundeira')
-    }
-  }
-
   render() {
     return (
       <div className="chat-header">
         <div className="chat-header__profile">
           <span className="chat-header__profile__name">Olá {this.props.username}!</span>
-          <span onClick={this.createRoom.bind(this)}> Nova sala</span>
         </div>
 
         {this.props.room && this.props.room.id &&
--- src/components/ChatList/index.js
+++ src/components/ChatList/index.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import NewChatModal from '../NewChatModal'
 
 export default class ChatList extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showModal: false
+    }
+  }
+
   selectChat(id) {
     this.props.socket.emit('join room', id)
+  }
+
+  toggleModal() {
+    this.setState({
+      showModal: !this.state.showModal
+    })
   }
 
   render() {
     return (
       <div className="chat-list">
+        <div onClick={this.toggleModal.bind(this)}>Chats +</div>
+
         {Object.keys(this.props.rooms).map(id => {
           return <span key={id} className="chat-list__item" onClick={this.selectChat.bind(this, id)}>{this.props.rooms[id].name}</span>
         })}
+
+        {this.state.showModal &&
+          <NewChatModal
+            toggleModal={this.toggleModal.bind(this)}
+            socket={this.props.socket} />
+        }
       </div>
     )
   }
