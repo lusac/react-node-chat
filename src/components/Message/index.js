8796769426031271b9f63bdbf9455e5164ec2c33
Lucas Chuairi Cruz <lucas.cruz@corp.globo.com>
2018-08-27 17:52:01 -0300

    Mensagem com novo formato de data + photo fake

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -4,20 +4,26 @@ import PropTypes from 'prop-types'
 export default class Message extends React.Component {
   formatDate(date) {
     let d = new Date(date)
-    return d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+    return `${d.toLocaleDateString()}, ${d.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}`
   }
 
   render() {
     return (
       <div className="message">
-        <strong className="message__info">
-          <span>{this.props.message.username}</span>
-          <span className="message__date">{this.formatDate(this.props.message.date)}</span>
-        </strong>
+        <div className="message__left">
+          <span className="message__photo">{this.props.message.username[0].toLocaleUpperCase()}</span>
+        </div>
 
-        <span className="message__text">
-          {this.props.message.text}
-        </span>
+        <div className="message__right">
+          <strong className="message__info">
+            <span>{this.props.message.username}</span>
+            <span className="message__date">{this.formatDate(this.props.message.date)}</span>
+          </strong>
+
+          <span className="message__text">
+            {this.props.message.text}
+          </span>
+        </div>
       </div>
     )
   }
